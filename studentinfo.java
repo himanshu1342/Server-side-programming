3b7354b4c965279a2a964bdd7c666256a546b834
Himanshu Chaudhari <138886967+himanshu1342@users.noreply.github.com>
2024-06-25 08:26:09 +0530

    Create studentinfo.java

---
+++ studentinfo.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class Studentinfo {
+
+	public static void main(String[] args) {
+		//ToDo Auto-generated method stub
+		Student stud = new Student();
+		stud.setName("John");
+		String s1=stud.getName();
+		System.out.println(s1);
+	}
+
+	}
+	
